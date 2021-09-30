bc61c4c242385bda5fc3c46ba3e37edb6da4bc35
paavill <pavelchist2001@gmail.com>
2021-09-30 23:23:54 +0300

    start layout

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,27 @@
+function openCity(evt, Name) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function setFirstActive(){
+    tablinks = document.getElementsByClassName("tablinks");
+    tablinks[0].className += " active";
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent[0].style.display = "block";
+}
