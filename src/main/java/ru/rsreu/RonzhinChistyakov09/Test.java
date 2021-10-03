3c137ab1a35297ea40212679c011fe1d53fbd915
paavill <pavelchist2001@gmail.com>
2021-10-04 00:32:11 +0300

    some fix

--- src/main/java/ru/rsreu/RonzhinChistyakov09/Test.java
+++ src/main/java/ru/rsreu/RonzhinChistyakov09/Test.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,6 +32,12 @@ public class Test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
+		Locale.setDefault(Locale.US);
+		try {
+			DAOFactory dao = DAOFactory.getInstance(DBType.ORACLE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		response.getWriter().print("This is " + this.getClass().getName() + ", using the GET method");
 	}
 
